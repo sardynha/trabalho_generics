41978235b0f26709865c39c5a227bc3098be5336
sarah <sahpacheco321@gmail.com>
2026-06-19 20:40:16 -0300

    iniciando_lista_encadeada

--- src/ListaEncadeada.java
+++ src/ListaEncadeada.java
@@ -1,3 +1,46 @@
-public class ListaEncadeada {
-    
+public class ListaEncadeada<T> implements Lista<T> {
+
+    private No<T> inicio;
+    private int tamanho;
+
+    public ListaEncadeada() {
+        inicio = null;
+        tamanho = 0;
+    }
+
+    @Override
+    public void add(T valor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public void add(T valor, int pos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public T get(int pos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public T remove(int pos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public boolean remove(T valor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
 }
