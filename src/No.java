6f3bdd838c160b5dc644f8acb0df47c8b2ec00f4
sarah <sahpacheco321@gmail.com>
2026-06-18 22:20:51 -0300

    no

--- src/No.java
+++ src/No.java
@@ -1,3 +1,11 @@
-public class No {
-    
+public class No<T> {
+
+    T valor;
+    No<T> proximo;
+    //guarda uma referencia para o proximo
+
+    public No(T valor) {
+        this.valor = valor;
+        this.proximo = null;
+    }
 }
